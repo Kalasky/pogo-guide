77fd447bd3b2d58190ffa22a72bfa3e8e815d078
Robert Kalasky <kalaskyr@gmail.com>
2023-01-04 17:48:27 -0500

    delete video card from DB

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from 'react'
 import ReactPlayer from 'react-player'
 import { Card, CardHeader, CardBody, CardFooter, Typography, Chip } from '@material-tailwind/react'
+import { useVideoContext } from '../hooks/useVideoContext'
 
 const VideoCard = ({ video }) => {
+  const { dispatch } = useVideoContext()
+
   const [showFullDescription, setShowFullDescription] = useState(false)
   const hasLongDescription = video.description.length > 150
+
+  const handleClick = async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/api/videos/${video._id}`, {
+        method: 'DELETE',
+      })
+      const json = await res.json()
+
+      if (res.ok) {
+        dispatch({ type: 'DELETE_VIDEO', payload: json })
+      }
+
+      console.log(json)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <Card className="w-96 m-7">
@@ -32,6 +52,7 @@ const VideoCard = ({ video }) => {
 
       <CardFooter divider className="flex items-center justify-between py-3">
         <Chip value={video.difficulty} color="amber" />
+        <Chip onClick={handleClick} value="delete" color="red" />
         <Typography variant="small" color="gray" className="flex gap-1">
           <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
           {video.createdAt.substring(0, 10)}
