9bfeaa8d63ceaab0de0b9acad700f9c00c766092
wiu <kalaskyr@gmail.com>
2023-01-16 10:56:04 -0500

    added modal describing peppers - added framer motion to manage transition

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -5,6 +5,9 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import { useVideoContext } from '../hooks/useVideoContext'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import { useModal } from './useModal'
+import Modal from './Modal'
+
 const VideoCard = ({ video }) => {
   const { dispatch } = useVideoContext()
   const [likes, setLikes] = useState(video.likes.length)
@@ -12,6 +15,8 @@ const VideoCard = ({ video }) => {
 
   const [showFullDescription, setShowFullDescription] = useState(false)
   const hasLongDescription = video.description.length > 150
+
+  const { isOpen, openModal, closeModal } = useModal()
 
   const handleClick = async () => {
     try {
@@ -77,7 +82,37 @@ const VideoCard = ({ video }) => {
           </div>
           <div className="flex items-center justify-between mt-6">
             <div className="flex items-center gap-2">
-              <span className="text-red-500 font-bold">{video.difficulty}</span>
+              <span className="text-red-500 font-bold cursor-pointer" onClick={openModal}>
+                {video.difficulty}
+              </span>
+              <Modal
+                isOpen={isOpen}
+                closeModal={closeModal}
+                title="Task Difficulty Rating"
+                message={
+                  <>
+                    Peppers are a widely used system to indicate the level of difficulty of a task or challenge. They are a quick
+                    and easy way for players to understand the level of difficulty they can expect from a task. Peppers can range
+                    from one pepper for easy tasks to seven peppers for nearly impossible tasks.
+                    <br />
+                    <br />
+                    Each post on the website has a creator rating and a community rating. The creator rating is assigned by the
+                    person who created the post and reflects their personal perception of the difficulty of the task. The
+                    community rating is the average rating assigned by users who have completed the task. These ratings are used
+                    to give players an idea of the difficulty level they can expect from a task, and to help them compare the
+                    difficulty level of different tasks.
+                    <br />
+                    <br />
+                    <span className="text-green-500 font-bold">1 Pepper</span> - Casual <br />
+                    <span className="text-yellow-500 font-bold">2 Peppers</span> - Novice <br />
+                    <span className="text-orange-500 font-bold">3 Peppers</span> - Intermediate <br />
+                    <span className="text-red-500 font-bold">4 Peppers</span> - Advanced <br />
+                    <span className="text-red-600 font-bold">5 Peppers</span> - Hardcore <br />
+                    <span className="text-red-700 font-bold">6 Peppers</span> - Elite <br />
+                    <span className="text-red-800 font-bold">7 Peppers</span> - Nightmare <br />
+                  </>
+                }
+              />
               <span className="text-white text-xs">{formatDistanceToNow(new Date(video.createdAt), { addSuffix: true })}</span>
               <span className="text-white text-xs">
                 <FontAwesomeIcon icon={faThumbsUp} onClick={handleLike} /> {likes}
